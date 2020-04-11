0ae2c6d99ac9fdc8990c25d94aea74bcc79fdbf3
jigargandhi <jigarr.gandhi@gmail.com>
2020-04-11 18:52:28 +0530

    Added test for previous bug

---
+++ Minsk.Tests/CodeAnalysis/Text/SourceTextTests.cs
@@ -0,0 +1,19 @@
+using Minsk.CodeAnalysis.Text;
+using Xunit;
+
+namespace Minsk.Tests.CodeAnalysis.Syntax
+{
+    public class SourceTextTests
+    {
+        [Theory]
+        [InlineData(".", 1)]
+        [InlineData(".\r\n", 2)]
+        [InlineData(".\r\n\r\n", 3)]
+        public void SourceText_IncludesLastLine(string text, int expectedLineCount)
+        {
+            var sourceText = SourceText.From(text);
+            Assert.Equal(expectedLineCount, sourceText.Lines.Length);
+            Assert.Equal(".", sourceText.Lines[0].ToString());
+        }
+    }
+}
